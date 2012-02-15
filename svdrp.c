a462e7397f1fe9d3da6954d84277f89584667a8c
Klaus Schmidinger <Klaus (dot) Schmidinger (at) tvdr (dot) de>
2012-02-15 16:59:00 +0100

    Version 1.6.0-3 - Changed cDvbDevice::GrabImage() to use V4L2   (backport from version 1.7.3, thanks to Ralf Schueler). - Added some missing 'const' keywords to avoid compilation errors with gcc 4.4   (backport from version 1.7.8, thanks to Ralf Schueler). - Modified cSVDRP::CmdGRAB() to avoid writing into const data   (backport from version 1.7.8, thanks to Ralf Schueler). - Fixed cRecordings::DelByName() to avoid compilation errors with gcc 4.4   (backport from version 1.7.9, thanks to Ralf Schueler).

--- svdrp.c
+++ svdrp.c
@@ -10,7 +10,7 @@
  * and interact with the Video Disk Recorder - or write a full featured
  * graphical interface that sits on top of an SVDRP connection.
  *
- * $Id: svdrp.c 1.109.1.1 2008/05/02 14:29:16 kls Exp $
+ * $Id: svdrp.c 1.109.1.3 2012/02/14 13:55:52 kls Exp $
  */
 
 #include "svdrp.h"
@@ -736,7 +736,7 @@ void cSVDRP::CmdGRAB(const char *Option)
      char *strtok_next;
      FileName = strtok_r(p, delim, &strtok_next);
      // image type:
-     char *Extension = strrchr(FileName, '.');
+     const char *Extension = strrchr(FileName, '.');
      if (Extension) {
         if (strcasecmp(Extension, ".jpg") == 0 || strcasecmp(Extension, ".jpeg") == 0)
            Jpeg = true;
@@ -795,16 +795,17 @@ void cSVDRP::CmdGRAB(const char *Option)
      char RealFileName[PATH_MAX];
      if (FileName) {
         if (grabImageDir) {
-           cString s;
-           char *slash = strrchr(FileName, '/');
+           cString s(FileName);
+           FileName = s;
+           const char *slash = strrchr(FileName, '/');
            if (!slash) {
               s = AddDirectory(grabImageDir, FileName);
               FileName = s;
               }
            slash = strrchr(FileName, '/'); // there definitely is one
-           *slash = 0;
-           char *r = realpath(FileName, RealFileName);
-           *slash = '/';
+           cString t(s);
+           t.Truncate(slash - FileName);
+           char *r = realpath(t, RealFileName);
            if (!r) {
               LOG_ERROR_STR(FileName);
               Reply(501, "Invalid file name \"%s\"", FileName);
